0d2fd37978e5d9f2434667f024bf9970118bc366
victoriaa <91256311+vikasatsik@users.noreply.github.com>
2021-10-14 21:51:26 +0300

    Добавьте файлы проекта.

---
+++ лаб4.1/лаб4.1.cpp
@@ -0,0 +1,43 @@
+﻿// лаб4.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int k, N, i;
+    double S;
+    cout << "k = "; cin >> k;
+    cout << "N = "; cin >> N;
+    S = 0;
+    i = k;
+    while (i <= N)
+    {
+        S += sin(i) / (1 + cos(i));
+        i++;
+    }
+    cout << S << endl;
+
+    S = 0;
+    i = k;
+    do {
+        S += sin(i) / (1 + cos(i));
+        i++;
+    } while (i <= N);
+    cout << S << endl;
+    S = 0;
+    for (i = k; i <= N; i++)
+    {
+        S += sin(i) / (1 + cos(i));
+    }
+    cout << S << endl;
+    S = 0;
+    for (i = N; i >= k; i--)
+    {
+        S += sin(i) / (1 + cos(i));
+    }
+    cout << S << endl;
+    return 0;
+}
+
